c28fe63391824ab65816edd784d7b29fc7307ade
W-Carlos <wendcarlos18@hotmail.com>
2023-01-31 19:53:27 -0300

    I made the routes to see and create users

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
+import express from "express"
 const app = express()
+app.use(express.json())
 
 const PORT = 3000
 
+const users = []
+
 app.get('/users', (request, response) => {
-   return response.send('Hollo Wolrd')
+   return response.json(users)
+})
+
+app.post('/user', (request, response) => {
+    const {name, email, telephone, address} = request.body
+
+    users.push({name, email, telephone, address})
+
+    return response.status(201).json({name, email, telephone, address})
 })
 
 app.listen(PORT)
